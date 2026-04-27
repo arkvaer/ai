85ae91dc0eb312d4956d05636454ec343cc9bd94
zhangchengtao <zhangchengtao@autohome.com.cn>
2026-04-27 19:34:51 +0800

    feat: add chatbox

---
+++ chat-model-vs-chat-client/src/main/java/com/arkvaer/chatmodelvschatclient/ChatModelVsChatClientApplication.java
@@ -0,0 +1,13 @@
+package com.arkvaer.chatmodelvschatclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatModelVsChatClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatModelVsChatClientApplication.class, args);
+	}
+
+}
